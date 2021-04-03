6448903a1bd21a9aa3bb94c69b6949ca91800760
Dubymar Tollinchi <dubymarjtr@gmail.com>
2021-04-03 11:49:03 -0500

    Enable submit button when user type the correct answer

--- scripts.js
+++ scripts.js
@@ -1,1 +1,9 @@
-
+document.getElementById("terms").addEventListener("keyup", () => {
+  const terms = document.getElementById("terms").value;
+  const submitButton = document.querySelector("button");
+  if (terms === "Yes" || terms === "yes") {
+    submitButton.disabled = false;
+  } else {
+    submitButton.disabled = true;
+  }
+});
